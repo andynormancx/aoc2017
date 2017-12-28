5a36d0a9afab426d8c2cab9d22ee25f43c77fd06
Andrew Norman <andy@norman.cx>
2017-12-28 17:21:34 +0000

    WIP JS

--- day23-2alt.js
+++ day23-2alt.js
@@ -4,43 +4,57 @@ function solve() {
     let b = 107900
     let c = 124900
     let d = 0
-    let e = 0
     let f = 0
     let g = 0
     let h = 0
 
     do {
-        f = 1// 8 set f 1
-        d = 2// 9 set d 2
+        f = 1 // 'set f 1', //9 - 
+        d = 2 // 'set d 2', //10 -
+
         do {
-            e = 2// 10 set e 2
+            e = 2 // 'set e 2', //11 - 
+
             do {
-                g = d// 11 set g d
-                g = g * e// 12 mul g e
-                g = g - b// 13 sub g b
-                if (g !== 0) {// 14 jnz g 2
-                    f = 0// 15 set f 0
-                }
-                e = e + 1// 16 sub e -1
-                g = e// 17 set g e
-                g = g - b// 18 sub g b
+                g = d // 'set g d', //12 - 
+                g = g * e // 'mul g e', //13 - 
+                g = g - b // 'sub g b', //14 - 
+                // 'jnz g 2', //15 - 
+                f = 0 // 'set f 0', //16 - 
+                e = e + 1// 'sub e -1', //17 - 
+                g = e // 'set g e', //18 - 
+                g = g - b // 'sub g b', //19 - 
             }
-            while (g != 0) // 19 jnz g -8
-            d = d + 1// 20 sub d -1
-            g = d// 21 set g d
-            g = g - b// 22 sub g b
+            while (g !== 0)// 'jnz g -8', //20 - 12
+
+            d = d + 1 // 'sub d -1', //21 - 
+            g = d // 'set g d', //22 - 
+            g = g - b // 'sub g b', //23 -
+            console.log({"a":a,"b":b,"c":c,"d":d,"e":e,"f":f,"g":g,"h":h})
+        }  
+        while(g !== 0)// 'jnz g -13', //24 - 11
+
+        if (f === 0 ) {// 'jnz f 2', //25 -
+            console.log('h++')
+            h = h + 1// 'sub h -1', //26 - 
         }
-        while (g != 0)// 23 jnz g -13
-        if (f == 0) {// 24 jnz f 2
-            h = h + 1// 25 sub h -1
+        g = b // 'set g b', //27 - 
+        console.log({
+            "a":a,
+            "b":b,
+            "c":c,
+            "d":d,
+            "e":e,
+            "f":f,
+            "g":g,
+            "h":h,
+        })
+        g = g - c // 'sub g c', //28 - 
+        if (g === 0) {// 'jnz g 2', //29 - 
+            console.log(h)
+            return// 'jnz 1 3', //30 - 
         }
-        g =  b// 26 set g b
-        g = g - c// 27 sub g c
-        if (g == 0) {// 28 jnz g 2
-            console.log('h: ' + h)
-            return// 29 jnz 1 3
-        }
-        b = b + 17// 30 sub b -17
+        b += 17 // 'sub b -17', //31 - 
     }
-    while(true)// 31 jnz 1 -23
+    while (true)// 'jnz 1 -23', //32 - 9
 }
